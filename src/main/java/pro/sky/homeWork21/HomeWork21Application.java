0d2f2373372ca5c5b64940b0345279e41816fa77
SaintEduis <edik.melk@yandex.ru>
2024-04-09 19:23:20 +0300

    First commit!

---
+++ src/main/java/pro/sky/homeWork21/HomeWork21Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWork21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork21Application.class, args);
+	}
+
+}
